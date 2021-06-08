7b01a3c126dabb8d549f6619b12186dd24c55071
FernandoYF <Fernandoyepezfelix@hotmail.com>
2021-06-07 23:33:37 -0500

    Ya arregle mi puerto:(

--- Parcial3/Login DB/Entrar.php
+++ Parcial3/Login DB/Entrar.php
@@ -1,21 +1,32 @@
 <?php
-session_start();
-include "Conexion.php";
-$conexion->query("SET NAMES 'UTF8'");
-$Usuario = $_POST['Usuario'];
-$contra = $_POST['contra'];
-$contra = md5($contra);
-$Login = $conexion->prepare("SELECT * FROM Usuario WHERE Nickname='$Usuario' and Contraseña='$contra'");
-$Login->execute();
-if ($Login->rowCount() > 0) {
-    $_SESSION['Usuario'] = $Usuario;
-    echo '
-        <script>
-        alert("HOLA, BIENVENIDO '.$_SESSION['Usuario'].'");
-        </script>
-        ';
-    header("Location: pagina.php");
+    session_start();
+    $server = "localhost"; 
+    $usuario = "root";
+    $contra = "Nando123";
+    $bd = "YEPEZFE";   
+    try
+    {
+        $conexion = new PDO("mysql:host=$server;dbname=$bd", $usuario, $contra);
+    }
+    catch(PDOException $error){
+        echo "Ha ocurrido un error al conectar a $bd";
+        echo $error->getMessage();
+        exit();
+    }
+    $conexion->query("SET NAMES 'UTF8'"); //si no no me trae correcta la variable:(
+    $Usuario = $_POST['Usuario'];
+    $contra = $_POST['contra'];
+    $contra = md5($contra);
+    $Login = $conexion->prepare("SELECT * FROM Usuario WHERE Nickname='$Usuario' and Contraseña='$contra'");
+    $Login->execute();
+    if ($Login->rowCount() > 0) {
+        $_SESSION['Usuario'] = $Usuario;
+        header("Location: pagina.php");
     exit;
+}
+else{
+        echo '<script language="javascript">alert("Contraseña o Usuario incorrectos:( intenta denuevo");</script>';
+        //header("Location: index.php");
 }
 ?>
 
--- Parcial3/Login DB/pagina.php
+++ Parcial3/Login DB/pagina.php
@@ -1,6 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de Bienvenida</title>
+</head>
+<body>
+    <h2>Inisio Sesión</h2>
+    <?php
         session_start();
-        
-        <p><a href="Bienvenido">:)</a></p>
-        <p><a href="Salir.php">Cerrar Sesión</a></p>
-?>
+        echo '<script language="javascript">alert("BIENVENIDO: '.$_SESSION['Usuario'].'");</script>';
+    ?>
+    <br>
+    <script type="text/javascript">
+        var Fecha = new Date();
+        document.write('Fecha: '+Fecha.getDate(),'<br>Dia de la semana: '+Fecha.getDay(),'<br>Número del Mes: '+Fecha.getMonth(),'<br>Año: '+Fecha.getFullYear(),'<br>Hora: '+Fecha.getHours(),'<br>Hora UTC: '+Fecha.getUTCHours(),'<br>Minutos: '+Fecha.getMinutes(),'<br>Segundos: '+Fecha.getSeconds());
+    </script>
+    <a href="Salir.php">Cerrar sesión</a>
+</body>
+</html>
